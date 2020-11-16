2bbe0b4c37ccc8306d7a0348b4b83c31a192f05b
Shavlovsii_Ivan <na613c613c@gmail.com>
2020-11-16 17:19:03 +0300

    [ADD] grouped by day, month, year with tabs and router

--- src/components/displayTypeTab/DisplayTypeTab.tsx
+++ src/components/displayTypeTab/DisplayTypeTab.tsx
@@ -1,41 +1,56 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import DateTab from './DateTab/DateTab';
 import StoreT from '../../models/StoreModel';
 import { Tabs } from 'antd';
 import { Typography } from 'antd';
 import { observer } from 'mobx-react';
-import { NavLink, Route } from 'react-router-dom';
+import { NavLink, Route, Switch } from 'react-router-dom';
 const { Title } = Typography;
 const { TabPane } = Tabs;
 
 type Props = { store: StoreT };
 
 const DisplayTypeTab: FunctionComponent<Props> = ({ store }) => {
+  const [state, setstate] = useState('1');
+
   return (
     <div className="card-container">
       <Title level={2}>Выберите тип отображения</Title>
-      <Tabs
-        defaultActiveKey="1"
-        onChange={(activeKey: string) => {
-          switch (activeKey) {
-            case '1':
-              return store.setTabDay();
-            case '2':
-              return store.setTabMonth();
-            default:
-              return store.setTabYear();
-          }
-        }}>
+      <Tabs activeKey={state}>
         <TabPane tab={<NavLink to="/day">День</NavLink>} key="1">
-          <Route path="/day" component={() => <DateTab store={store} />}></Route>
+          <DateTab store={store} />;
         </TabPane>
         <TabPane tab={<NavLink to="/month">Месяц</NavLink>} key="2">
-          <Route path="/month" component={() => <DateTab store={store} />}></Route>
+          <DateTab store={store} />;
         </TabPane>
         <TabPane tab={<NavLink to="/year">Год</NavLink>} key="3">
-          <Route path="/year" component={() => <DateTab store={store} />}></Route>
+          <DateTab store={store} />;
         </TabPane>
       </Tabs>
+
+      <Switch>
+        <Route
+          path="/day"
+          component={() => {
+            setstate('1');
+            store.setTabDay();
+            return <></>;
+          }}></Route>
+        <Route
+          path="/month"
+          component={() => {
+            setstate('2');
+            store.setTabMonth();
+            return <></>;
+          }}></Route>
+        <Route
+          path="/year"
+          component={() => {
+            setstate('3');
+            store.setTabYear();
+            return <></>;
+          }}></Route>
+      </Switch>
     </div>
   );
 };
